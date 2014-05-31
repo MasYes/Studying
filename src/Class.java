33cb70507b6329566437d4fd73b379d8fdc8a588
MasYes <bymasyes@gmail.com>
2014-06-01 02:37:12 +0400

    2

--- src/Class.java
+++ src/Class.java
@@ -1,5 +1,12 @@
-public class Class {
+public class Class{
 	public static void main(String[] args){
-		System.out.println("Hello world!");
+		int i = 5;
+		int j = 10;
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(i + j);
+		System.out.println(j - i);
+		System.out.println(i * j);
+		System.out.println(j / i);
 	}
 }
